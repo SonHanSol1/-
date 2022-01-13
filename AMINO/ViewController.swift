a81e1fe3cd5186179aa26607d01cf2fe7b6de687
SonHanSol1 <gksthf7869@naver.com>
2022-01-13 14:33:48 +0900

    Initial Commit

---
+++ AMINO/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AMINO
+//
+//  Created by 손한솔 on 2022/01/13.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
